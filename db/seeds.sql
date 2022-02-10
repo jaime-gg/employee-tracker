bdf3949463b0881d00843035ff7ad6a121733133
jester <jaimebuddy98@gmail.com>
2022-02-10 12:53:38 -0600

    added content to the db seeds

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO department (name)
+VALUES
+('IT'),
+('Sales'),
+('Accounting'),
+('Human Resources');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Web Developer', 70000, 1),
+('Full Stack Web Developer', 75000, 1),
+('Sales Associate', 70000, 2),
+('Sales Manager', 75000, 2),
+('Accountant', 70000, 3),
+('Finance Manager', 75000, 3),
+('HR Director', 75000, 4),
+('Compensation Manager', 70000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Jack', 'Jackson', 1, 2),
+('Jill', 'Jilleon', 2, NULL),
+('Bob', 'Bigget', 3, 4),
+('Barb', 'Bregerton', 4, NULL),
+('Mac', 'Martinez', 5, 6),
+('Marcy', 'Miler', 6, NULL),
+('Rachel', 'Ramierez', 7, NULL),
+('Rob', 'Red', 8, NULL);
--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const mysql = require('mysql2')
 const inquirer = require('inquirer'); 
 const cTable = require('console.table'); 
 
-const db = require('./db/connection');
+const db = require('./db/connection.js');
 
+//-------------------------------------------------------------------------------------------------
+
+// USE THE DB/CONNECTIONS.JS TO START THE SCHELL
 db.connect(err => {
     if (err) throw err;
     console.log('connected as id ' + connection.threadId);
