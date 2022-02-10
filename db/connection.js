f72d07bdf28d093b0aa273c4008f1f902ab59712
jester <jaimebuddy98@gmail.com>
2022-02-10 13:23:28 -0600

    included password with .env

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,15 @@
-// dependency 
+// dependencies
 const mysql = require("mysql2/promise");
+require('dotenv').config()
 
 const db = mysql.createConnection(
-        {
-            host: 'localhost',
-            // Your MySQL username,
-            user: root,
-            // Your MySQL password
-            password: ,
-            database: 'company'
-        })
-
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: root,
+        // Your MySQL password
+        password: process.env.PASS,
+        database: 'company'
+    })
 
 module.exports = db
